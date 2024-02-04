c6d38cfb8c6e2039a0d707f46b956c61406b5d54
Zayynid <158794442+Zayynid@users.noreply.github.com>
2024-02-04 11:07:56 +0700

    Add files via upload
    upload files

---
+++ script.js
@@ -0,0 +1,40 @@
+const navbarNav = document.querySelector('#navmenu');
+
+document.querySelector('#menubtn').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+
+const menu = document.querySelector('#menubtn');
+
+document.addEventListener('click', function(e) {
+    if(!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+// product card section
+var popupViews = document.querySelectorAll('.popup-view');
+var popupBtns = document.querySelectorAll('.popup-btn');
+var closeBtns = document.querySelectorAll('.close-btn');
+
+//javascript for quick view button
+var popup = function(popupClick){
+  popupViews[popupClick].classList.add('active');
+}
+
+popupBtns.forEach((popupBtn, i) => {
+  popupBtn.addEventListener("click", () => {
+    popup(i);
+  });
+});
+
+//javascript for close button
+closeBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener("click", () => {
+    popupViews.forEach((popupView) => {
+      popupView.classList.remove('active');
+    });
+  });
+});
